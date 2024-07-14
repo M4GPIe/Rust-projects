93dace0cfe868f6e80fc44fa2b62bf8efe97cac8
M4GPIe <delossantossanchismanuel@gmail.com>
2024-07-14 21:26:12 +0200

    hello world and hello cargo

---
+++ learning_exercises/02_Hello_Cargo/target/debug/Hello_Cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Admin\OneDrive\Escritorio\Lenguajes\Rust\Rust-projects\learning_exercises\02_Hello_Cargo\target\debug\Hello_Cargo.exe: C:\Users\Admin\OneDrive\Escritorio\Lenguajes\Rust\Rust-projects\learning_exercises\02_Hello_Cargo\src\main.rs
---
+++ learning_exercises/02_Hello_Cargo/target/debug/deps/Hello_Cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\OneDrive\Escritorio\Lenguajes\Rust\Rust-projects\learning_exercises\02_Hello_Cargo\target\debug\deps\Hello_Cargo.exe: src/main.rs
+
+C:\Users\Admin\OneDrive\Escritorio\Lenguajes\Rust\Rust-projects\learning_exercises\02_Hello_Cargo\target\debug\deps\Hello_Cargo.d: src/main.rs
+
+src/main.rs:
